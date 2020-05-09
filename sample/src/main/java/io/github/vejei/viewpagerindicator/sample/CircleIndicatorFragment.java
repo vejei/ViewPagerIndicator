bf1170b5088f15f33ebbfbdd26947daab59298e4
vejei <chengsnem@163.com>
2020-05-09 18:04:25 +0800

    sample fagments

---
+++ sample/src/main/java/io/github/vejei/viewpagerindicator/sample/CircleIndicatorFragment.java
@@ -0,0 +1,58 @@
+package io.github.vejei.viewpagerindicator.sample;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.Arrays;
+
+import io.github.vejei.viewpagerindicator.indicator.CircleIndicator;
+
+public class CircleIndicatorFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_circle_indicator, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        MainActivity activity = (MainActivity) getActivity();
+        if (activity == null) return;
+
+        PageAdapter pageAdapter = new PageAdapter();
+        pageAdapter.setData(Arrays.asList(activity.data));
+        ViewPager2 viewPager2 = view.findViewById(R.id.view_pager2);
+        viewPager2.setAdapter(pageAdapter);
+
+        CircleIndicator slideAnimationIndicator = view.findViewById(R.id.circle_indicator_slide);
+        slideAnimationIndicator.setWithViewPager2(viewPager2, false);
+        slideAnimationIndicator.setItemCount(activity.data.length);
+        slideAnimationIndicator.setAnimationMode(CircleIndicator.AnimationMode.SLIDE);
+
+        CircleIndicator scaleAnimationIndicator = view.findViewById(R.id.circle_indicator_scale);
+        scaleAnimationIndicator.setWithViewPager2(viewPager2, false);
+        scaleAnimationIndicator.setItemCount(activity.data.length);
+        scaleAnimationIndicator.setAnimationMode(CircleIndicator.AnimationMode.SCALE);
+
+        CircleIndicator colorAnimationIndicator = view.findViewById(R.id.circle_indicator_color);
+        colorAnimationIndicator.setWithViewPager2(viewPager2, false);
+        colorAnimationIndicator.setItemCount(activity.data.length);
+        colorAnimationIndicator.setAnimationMode(CircleIndicator.AnimationMode.COLOR);
+
+        CircleIndicator noAnimationIndicator = view.findViewById(R.id.circle_indicator_none);
+        noAnimationIndicator.setWithViewPager2(viewPager2, false);
+        noAnimationIndicator.setItemCount(activity.data.length);
+        noAnimationIndicator.setAnimationMode(CircleIndicator.AnimationMode.NONE);
+    }
+}
---
+++ sample/src/main/java/io/github/vejei/viewpagerindicator/sample/RectIndicatorFragment.java
@@ -0,0 +1,50 @@
+package io.github.vejei.viewpagerindicator.sample;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
+
+import java.util.Arrays;
+
+import io.github.vejei.viewpagerindicator.indicator.RectIndicator;
+
+public class RectIndicatorFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_rect_indicator, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        MainActivity activity = (MainActivity) getActivity();
+        if (activity == null) return;
+
+        PageAdapter pageAdapter = new PageAdapter();
+        pageAdapter.setData(Arrays.asList(activity.data));
+        ViewPager2 viewPager2 = view.findViewById(R.id.view_pager2);
+        viewPager2.setAdapter(pageAdapter);
+
+        RectIndicator rectIndicator = view.findViewById(R.id.rect_indicator);
+        rectIndicator.setWithViewPager2(viewPager2, false);
+        rectIndicator.setItemCount(activity.data.length);
+
+        RectIndicator squareIndicator = view.findViewById(R.id.square_indicator);
+        squareIndicator.setWithViewPager2(viewPager2, false);
+        squareIndicator.setItemCount(activity.data.length);
+
+        RectIndicator stadiumIndicator = view.findViewById(R.id.stadium_indicator);
+        stadiumIndicator.setWithViewPager2(viewPager2, false);
+        stadiumIndicator.setItemCount(activity.data.length);
+    }
+}
