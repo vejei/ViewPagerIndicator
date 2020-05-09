ba73dd0553e2bcc8701c22ce0a43908f12295e7b
vejei <chengsnem@163.com>
2020-05-09 18:01:42 +0800

    Change root layout

--- sample/src/main/java/io/github/vejei/viewpagerindicator/sample/MainActivity.java
+++ sample/src/main/java/io/github/vejei/viewpagerindicator/sample/MainActivity.java
@@ -1,14 +1,31 @@
 package io.github.vejei.viewpagerindicator.sample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    String[] data = {
+            "First", "Second", "Third", "Fourth", "Fifth", "Sixth"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container, new MainFragment())
+                    .commit();
+        }
+    }
+
+    public void addFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.fragment_container, fragment)
+                .addToBackStack(null)
+                .commit();
     }
 }
